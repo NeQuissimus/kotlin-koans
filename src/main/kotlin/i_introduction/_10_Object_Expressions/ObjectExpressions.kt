b84b5d3edbd2bae8072f51f42290582b98ac447c
Tim Steinbach <steinbach.tim@googlemail.com>
2015-12-10 23:18:11 +0000

    I/10 Object Expressions

--- src/main/kotlin/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/main/kotlin/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -17,6 +17,8 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(s: Int, t: Int): Int = if (s > t) -1 else if (s == t) 0 else 1
+    })
     return arrayList
 }
